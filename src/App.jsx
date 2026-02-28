88ab37483238ccb44b9a53ac7f460806651a907d
IgorMartins0729 <igor.mrt02@gmail.com>
2026-02-28 17:52:00 -0300

    update

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './components/Header/Header'
+
+function App() {
+
+  return (
+    <main>
+      <Header />
+      {/* Comentando os componentes que ainda não foram criados para não quebrar o app: */}
+      {/* <InfoProfile /> */}
+      {/* <AboutMe /> */}
+      {/* <Academic /> */}
+      {/* <Projects /> */} {/* Ah, corrigi um errinho de digitação aqui, estava Projecs rs */}
+      {/* <Skills /> */}
+      {/* <Certificates /> */}
+      {/* <Contact /> */}
+    </main>
+  )
+}
+
+export default App
